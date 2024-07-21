441fd6ad4de09db6abd3b2d66d5736d0c59baa6d
ana-isabel-rodriguez <anaisabelrodiguez@gmail.com>
2024-07-21 22:16:11 +0200

    add invoice and spent models

---
+++ back/models/invoice.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const InvoiceSchema = new Schema({
+  usuarioId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true
+  },
+  empresa: {
+    type: String,
+    required: true
+  },
+  fecha: {
+    type: Date,
+    required: true
+  },
+  monto: {
+    type: Number,
+    required: true
+  },
+  archivo: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('invoice', InvoiceSchemaSchema);
