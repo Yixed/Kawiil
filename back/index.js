08a6e41f90c7f2f8c4d683ea3c43cfce5a5978af
ana-isabel-rodriguez <anaisabelrodiguez@gmail.com>
2024-07-21 23:30:39 +0200

    review index

--- back/index.js
+++ back/index.js
@@ -3,21 +3,18 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require ("cors")
 
-
-// Importar rutas
 const userRoutes = require('./routes/user.routes');
-const bookingRoutes = require('./routes/booking.routes');
-const vehicleRoutes = require('./routes/vehicle.routes');
+const reportsRoutes = require('./routes/reports.routes');
+const spentRoutes = require('./routes/spent.routes');
+const spentInvoices = require('./routes/invoices.routes');
 
-dotenv.config(); // Cargar variables de entorno desde .env
+dotenv.config();
 const app = express();
 
-// Middleware para parsear JSON
 app.use(express.json());
-//Deshabilito Cors Policy
+
 app.use(cors())
 
-// Conexión a MongoDB Atlas
 const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
@@ -26,12 +23,11 @@ mongoose.connect(MONGO_URI, {
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch(err => console.error('Error connecting to MongoDB', err));
 
-// Definir rutas
-app.use('/api/users', userRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/vehicles', vehicleRoutes);
+app.use('/api/invoices', invoicesRoutes);
+app.use('/api/reports', reportsRoutes);
+app.use('/api/spent', spentRoutes);
+app.use('/api/user', userRoutes);
 
-// Definir el puerto y poner en marcha el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
