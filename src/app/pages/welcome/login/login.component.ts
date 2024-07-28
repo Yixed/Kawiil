9987d2d82af87448bf2c38bec28d71e6968c7df9
Floren González <florengv6@gmail.com>
2024-07-28 22:28:51 +0200

    login form upgraded, still wip

--- src/app/pages/welcome/login/login.component.ts
+++ src/app/pages/welcome/login/login.component.ts
@@ -1,30 +1,60 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { WelcomeNavBarComponent } from "../../../components/commons/welcome-nav-bar/welcome-nav-bar.component";
-import { FooterComponent } from "../../../components/commons/footer/footer.component";
-import { RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { WelcomeNavBarComponent } from '../../../components/commons/welcome-nav-bar/welcome-nav-bar.component';
+import { FooterComponent } from '../../../components/commons/footer/footer.component';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { User } from '../../../interfaces/user';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ RouterModule , ReactiveFormsModule, WelcomeNavBarComponent, FooterComponent],
+  imports: [
+    RouterModule,
+    ReactiveFormsModule,
+    WelcomeNavBarComponent,
+    FooterComponent,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  passwordType: string = 'password';
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.form = fb.group({
       email: new FormControl(null, [Validators.required, Validators.email]),
-      pass: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
     });
   }
 
-  submitForm() {
-    console.log(this.form.value);
+  changePasswordSight(pass_: String) {
+    this.passwordType = pass_ === 'text' ? 'password' : 'text';
   }
 
-
+  submitForm() {
+    console.log('clicado, valores del formulario: ', this.form.value);
+    this.authService
+      .login(this.form.value.email, this.form.value.password)
+      .subscribe({
+        next: (response) => {
+          this.authService.saveUser(response as User);
+          this.router.navigate(['/home']);
+        },
+        error: () => {
+          console.log('error on login');
+        },
+      });
+  }
 }
