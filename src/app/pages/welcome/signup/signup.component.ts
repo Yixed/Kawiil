b7c47cb303d27f24c6eedb551b40f6a7f8f5e69f
anderplacencia <anderson.jose.placencia@bootcamp-upgrade.com>
2024-07-29 08:56:05 +0200

    Register ok

--- src/app/pages/welcome/signup/signup.component.ts
+++ src/app/pages/welcome/signup/signup.component.ts
@@ -1,15 +1,42 @@
+import { AuthService } from './../../../services/auth.service';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Route, Router, RouterModule } from '@angular/router';
 import { WelcomeNavBarComponent } from "../../../components/commons/welcome-nav-bar/welcome-nav-bar.component";
 import { FooterComponent } from "../../../components/commons/footer/footer.component";
+import { FormGroup, FormBuilder, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterModule, WelcomeNavBarComponent, FooterComponent],
+  imports: [RouterModule, WelcomeNavBarComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  form!: FormGroup
+
+  constructor(private builder: FormBuilder, 
+              private authService: AuthService,
+              private router: Router){
+   this.form = builder.group({
+    name: new FormControl(null, [Validators.required]),
+    surname: new FormControl(null, [Validators.required]), 
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    pwd: new FormControl(null, [Validators.required,Validators.minLength(9)]),
+   }) 
+  }
+
+  signup() {
+    console.log(this.form.value)
+    this.authService.signup(this.form.value.name, this.form.value.surname,this.form.value.email, this.form.value.pwd).subscribe({
+      next: () => {
+        this.router.navigateByUrl("/login")
+      },
+      error: () =>{
+        alert ("Rellena todo el formulario")
+      }      
+    })
+    
+  }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
+import { ParseSourceFile } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  url: string = 'https://dummyjson.com/auth';
+  url: string = 'https://backkawiilbd-1.onrender.com/user/';
   user: User|null = null;
   
   constructor(private http: HttpClient) {}
@@ -18,6 +19,20 @@ export class AuthService {
       password: password_,
     });
   } 
+
+  signup(name: string, surname: string, email: string,  pwd: string){
+    return this.http.post(
+      `${this.url}/addUser`,
+      {
+        name: name,
+        surname: surname,
+        email: email,
+        password: pwd
+      }
+    
+  )
+
+  }
 
   saveUser(user: User){
     this.user = user;
